6b8acf7d8fce4902ccd8122e425f8482a4f0dbb4
ChSivamowny15 <chsivamowny@gmail.com>
2026-07-29 14:45:22 +0530

    Add Max_Min.java to display integer limits

---
+++ Max_Min.java
@@ -0,0 +1,12 @@
+// Maximum & Minimum
+
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Maximum Integer: " + Integer.MAX_VALUE);
+        System.out.println("Minimum Integer: " + Integer.MIN_VALUE);
+    }
+}
