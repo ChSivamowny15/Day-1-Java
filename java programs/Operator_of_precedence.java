01d362a274e50c3d6e6d84185694d053067eefc0
ChSivamowny15 <chsivamowny@gmail.com>
2026-07-30 02:40:37 +0530

    Delete java programs/Operator_of_precedence.java

--- java programs/Operator_of_precedence.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class Operator_of_precedence {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        x = -6+7*8%7+6;
-        System.out.println("x is :" x);
-    }
-}
